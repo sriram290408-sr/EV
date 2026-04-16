1135aa6663a59bb10ae01a00ba4729856e99bd0a
sriramesakkimuthu-art <sriram290408@gmail.com>
2026-04-16 14:50:24 +0530

    fix in db

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,17 @@
 import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+load_dotenv()
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+if DATABASE_URL is None:
+    raise ValueError(
+        "DATABASE_URL is missing. Check your .env file location and value."
+    )
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
